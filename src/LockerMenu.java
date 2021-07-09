ea8f811b51a2849d6f193edeaa59e28929bcd30d
S, Disha <disha_s@dell.com>
2021-07-09 10:26:45 +0530

    complete

--- src/LockerMenu.java
+++ src/LockerMenu.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
-
 import com.lockme.model.Users;
+import com.lockme.model.UserCredentials;
 
 public class LockerMenu {
 	
 	File users;
 	File file;
-	File userFile;
+	File userDirectory;
 	Scanner in;
 	private static Users userobj;
+	private static UserCredentials usercred;
 	String basePath = "LockMe/";
 	private static Scanner fileReader;
 	
@@ -41,6 +42,9 @@ public class LockerMenu {
 			else {
 				System.out.println("Application Setup Successful");
 			}	
+			
+			userobj = new Users();
+			usercred = new UserCredentials();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -73,13 +77,9 @@ public class LockerMenu {
 				break;
 		default: System.out.println("Sorry!! invalid input");
 				 break;
-		}
+		}		
 		
-		System.out.println("        Main  Menu              ");
-		System.out.println("1. List all the Credentials");
-		System.out.println("2. Modify the credentials");
-		System.out.println("3. Close the application");
-		
+		in1.close();
 		
 	}
 	
@@ -87,7 +87,8 @@ public class LockerMenu {
 		try {
 		
 		System.out.println("--------------------------------");
-		System.out.println("");
+		System.out.println("");		
+		
 		System.out.println("Enter the Username");
 		String username = in.nextLine();
 		userobj.setUsername(username);
@@ -99,12 +100,22 @@ public class LockerMenu {
 		System.out.println("Username:"+ username);
 		System.out.println("Password:"+ password);
 		
-		FileWriter writer = new FileWriter(users);
-		writer.write(username);
-		writer.write(password);
+		userDirectory = new File(basePath + username);
+		FileWriter writer = new FileWriter(users, true);
 		
-		System.out.println("User Registered successfully");
 		
+		if(!userDirectory.exists()) {
+			
+			userDirectory.mkdir();			
+						
+			writer.write(username + "\n");
+			writer.write(password + "\n");
+			System.out.println("User Registered successfully");
+		}
+		else {
+			System.out.println("User already exists");
+		}
+				
 		writer.close();
 		
 		}catch(Exception e) {
@@ -116,8 +127,6 @@ public class LockerMenu {
 public void SignIn() {
 	
 	try {
-		
-		Scanner in = new Scanner(System.in);
 		
 		System.out.println("--------------------------------");
 		System.out.println("");
@@ -134,9 +143,13 @@ public void SignIn() {
 				System.out.println("Enter Password :");
 				String Password = in.nextLine();
 				if(fileReader.next().equals(Password)) {
+					
 					System.out.println("Login Successful ! 200OK");
 					found = true;
+					userDirectory = new File(basePath + username);
+					basePath = basePath + username + "/";
 					lockerOptions(username);
+					
 					break;
 				}
 			}
@@ -151,21 +164,204 @@ public void SignIn() {
 	}
 
 	public void lockerOptions(String inpUsername) {
-		System.out.println("1 . FETCH ALL STORED CREDENTIALS ");
-		System.out.println("2 . STORED CREDENTIALS ");
+		
+		while(true) {
+			
+			System.out.println("==========================================");
+			System.out.println("");
+			System.out.println("        Locker Options              ");
+			System.out.println("1. List all the Credentials");
+			System.out.println("2. Modify the credentials");
+			System.out.println("3. Close the application");
+			int option = in.nextInt();
+			switch(option) {
+				case 1 : 
+					listAscending(inpUsername);
+					break;
+				case 2 :
+					showModifyOption(inpUsername);
+					break;
+				case 3 :
+					closeApplication(inpUsername);
+					break;
+				default :
+					System.out.println("Please select 1, 2 or 3");
+					break;
+			}
+			
+		}
+		
+	}
+
+	private void closeApplication(String inpUsername) {
+		
+		in.close();
+		System.exit(0);
+		
+	}
+
+	private void showModifyOption(String inpUsername) {
+		
+		System.out.println("==========================================");
+		System.out.println("");
+		System.out.println("1 . ADD CREDENTIAL");
+		System.out.println("2 . DELETE CREDENTIAL ");
+		System.out.println("3 . SEARCH CREDENTIAL ");
+		System.out.println("4 . RETURN TO LOCKER OPTIONS ");
+		
 		int option = in.nextInt();
 		switch(option) {
 			case 1 : 
-				fetchCredentials(inpUsername);
-				break;
-			case 2 :
 				storeCredentials(inpUsername);
 				break;
+			case 2 :
+				deleteCredentials(inpUsername);
+				break;
+			case 3 :
+				fetchCredentials(inpUsername);
+				break;
+			case 4 :
+				return;
 			default :
 				System.out.println("Please select 1 Or 2");
 				break;
 		}
-		lockerInput.close();
+	}
+
+	private void listAscending(String inpUsername) {
+		
+		System.out.println("==========================================");
+		System.out.println("*					*");
+		System.out.println("*   WELCOME TO DIGITAL LOCKER 	 *");
+		System.out.println("*   YOUR CREDS ARE 	 *");
+		System.out.println("*					*");
+		System.out.println("==========================================");
+		
+		String[] list = userDirectory.list();
+		
+		if(list != null) {
+			
+			for( String item : list) {
+				
+				System.out.println(item);
+			}
+		}
+		else {
+			System.out.println("\n No credentials entered yet");
+		}
+		
+	}
+
+	private void deleteCredentials(String inpUsername) {
+		
+		System.out.println("Please enter the sitename of the credential to be deleted");
+		String sitename = in.next();
+		
+		File siteFile = new File(basePath + sitename + ".txt");
+		
+		if(siteFile.exists()) {
+			siteFile.delete();
+			System.out.println(" credentials deleted successfully");
+		}
+		else {
+			System.out.println(" credentials does not exist");
+		}
+		
+	}
+
+	private void storeCredentials(String inpUsername) {
+
+		try {
+			
+			System.out.println("==========================================");
+			System.out.println("*					*");
+			System.out.println("*   WELCOME TO DIGITAL LOCKER STORE YOUR CREDS HERE	 *");
+			System.out.println("*					*");
+			System.out.println("==========================================");
+			
+			usercred.setLoggedInUser(inpUsername);
+			
+			System.out.println("Enter Site Name :");
+			String siteName = in.next();
+			usercred.setSiteName(siteName);
+			
+			System.out.println("Enter Username :");
+			String username = in.next();
+			usercred.setUsername(username);
+			
+			System.out.println("Enter Password :");
+			String password = in.next();
+			usercred.setPassword(password);
+			
+			File site = new File(basePath + siteName + ".txt");
+			
+			if(!site.exists()) {
+				site.createNewFile();
+			}
+			
+			FileWriter WriteCred = new FileWriter(site);
+			
+			WriteCred.write(usercred.getLoggedInUser() + "\n");
+			WriteCred.write(usercred.getSiteName() + "\n");
+			WriteCred.write(usercred.getUsername() + "\n");
+			WriteCred.write(usercred.getPassword() + "\n");
+			
+			System.out.println("YOUR CREDS ARE STORED AND SECURED!");
+			WriteCred.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+	}
+
+	private void fetchCredentials(String inpUsername) {
+		
+		System.out.println("==========================================");
+		System.out.println("*					*");
+		System.out.println("*   WELCOME TO DIGITAL LOCKER 	 *");
+		System.out.println("*   YOUR CREDS ARE 	 *");
+		System.out.println("*					*");
+		System.out.println("==========================================");
+		
+		System.out.println("Enter Site Name :");
+		String siteName = in.next();
+		usercred.setSiteName(siteName);
+		
+		
+		for( String item : userDirectory.list()) {
+		
+			try {
+				
+				if(item.contains(siteName)) {
+					
+					File temp = new File(basePath + item);		
+					
+					fileReader = new Scanner(temp);
+					
+					while(fileReader.hasNext()) { 
+						
+						String usertemp  = fileReader.next();
+						 if(usertemp.equals(inpUsername)) {
+							  
+							  System.out.println("Site Name: "+fileReader.next());
+							  System.out.println("User Name: "+fileReader.next());
+							  System.out.println("User Password: "+fileReader.next()); 
+						 } 
+					}	
+							 
+				}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}			
+			
+		}
+		
+		
+		 
+		 
+		
 	}
 
 }
